b67a68d242de4527972615bd4328440fc416add6
dasushan <53913896+dasushan@users.noreply.github.com>
2024-10-18 01:06:49 +0530

    making the mails realtime

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -12,6 +12,45 @@ const Messages = () => {
   const sent = useSelector((state) => state.emails.sent);
   const dispatch = useDispatch();
 
+  // Define polling function
+  const pollApi = async () => {
+    try{
+      const email = emailId ? emailId.replace(/[@ .]/g, '') : '';
+      const response = await fetch(`https://react-backend-app-f330f-default-rtdb.asia-southeast1.firebasedatabase.app/${email}.json`, {
+        method: 'GET'
+      })
+      const result = response.json();
+      if( result && result.inbox){
+        const data = [];
+        for(const [key, value] of Object.entries(result.inbox)){
+          data.push(value);
+        }
+        dispatch(setEmails(data));
+      }
+      if( result && result.sent){
+        const data = [];
+        for(const [key, value] of Object.entries(result.sent)){
+          data.push(value);
+        }
+        dispatch(setSent(data));
+      }
+    }catch(e){
+      console.log(e.message);
+    }
+  }
+
+  useEffect(()=> {
+    // Start polling when the component mounts
+    const interval = setInterval(() => {
+      pollApi();
+      console.log(`pollApi ran`)
+    }, 2000);    // Poll every two seconds
+    
+    // Cleanup the interval on unmount
+    return () => clearInterval(interval);
+  }, [])
+
+
   useEffect(() => {
     const email = emailId ? emailId.replace(/[@ .]/g, '') : '';
     fetch(
